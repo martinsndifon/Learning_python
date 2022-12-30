debbea0d353348bf5162148d78fdb6670fb76c1c
martinsndifon <martinsndifon@gmail.com>
2022-12-30 15:50:26 +0100

    string indexing with python loops

---
+++ string_indexing.py
@@ -0,0 +1,8 @@
+name = "martins.ndifon? mgbe:evareh emmanuel. michael?ifere"
+
+for i in name:
+	if i == '.' or i == '?' or i == ':':
+		print(f'{i}', end='')
+		print('\n')
+	else:
+		print(f'{i}', end='')
