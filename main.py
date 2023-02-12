09cd290c019d9cf45f649a7ad519bfb261bc5ea4
martinsndifon <martinsndifon@gmail.com>
2023-02-12 16:15:59 +0100

    keys, values and items

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-print("Hello, world")
+
+obj = {'name': "Martins",
+	   'age': 27, 
+       'height': 172.2, 
+       'skincolor': "Black", 
+       'hobbies':["chess", "swimming", "coding", "tiktoks"]
+    }
+print("Keys and values:")
+for key, val in obj.items():
+    print(f"{key}: {val}")
+print("values:")
+for val in obj.values():
+    print(f"{val}")
+print("keys:")
+for key in obj.keys():
+    print(f"{key}")
+
+print(obj)
+print(obj["hobbies"])
+
+for key in obj:
+    print(key)
